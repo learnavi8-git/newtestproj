45c2982584dd199a42a848faa3a3bf63df07ea53
learnavi8-git <learnavi8@gmail.com>
2021-01-01 18:49:18 +0530

    Initial Commit

---
+++ src/test/java/com/example/newtestproj/NewtestprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newtestproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewtestprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
